0fc6fe6d32d212e06e16df6ccccb9d4ea56335ad
AminMSD <msd.amin1374@gmail.com>
2016-08-08 18:48:44 +0430

    solution section hover

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
-$(window).on('load',setTimeout(function () {
+$(window).ready(setTimeout(function () {
     $(".loading").fadeOut(600);
-    $(".content").css({"display":"block"});
-},6000));
+    $(".content").css({"display": "block"});
+}, 6000));
+
+function hoverLeft() {
+//     // alert('aaa');
+//     $("#web").css({'width': '55%'});
+//     $("#app").css({'width': '45%'});
+//     $(".slant-container").css({"margin-left": "5%"});
+    $("#web").removeClass("decreaseWidth");
+    $("#web").addClass("increaseWidth");
+    $("#app").removeClass("increaseWidth");
+    $("#app").addClass("decreaseWidth");
+};
+function hoverRight() {
+//     // alert('aaa');
+//     $("#app").css({'width': '55%'});
+//     $("#web").css({'width': '45%'});
+//     $(".slant-container").css({"margin-right": "5%"});
+    $("#app").removeClass("decreaseWidth");
+    $("#app").addClass("increaseWidth");
+    $("#web").removeClass("increaseWidth");
+    $("#web").addClass("decreaseWidth");
+};
