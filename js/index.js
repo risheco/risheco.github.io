144f879f10c00e1e664d4e0aeb9fe6f6e5e50adb
Amirmohammad Moradi <amirmohammad@irsapardaz.ir>
2016-08-10 18:56:28 +0430

    about ..

--- js/index.js
+++ js/index.js
@@ -28,19 +28,60 @@ function hoverRight() {
 };
 
 
-$("section#members ul.membersList li.member").on('mouseenter',function () {
+$("section#members ul.membersList li.member").on('mouseenter', function () {
     $(this).children(".memberInfo").stop().fadeIn();
 });
-$("section#members ul.membersList li.member").on('mouseleave',function () {
+$("section#members ul.membersList li.member").on('mouseleave', function () {
     $(this).children(".memberInfo").stop().hide();
 });
 
-var sectionHeight = $("section#intro").height();
-$(document).on('scroll',function () {
-    if ($(document).scrollTop() > 100){
+var sectionHeight = $("section").height();
+$("#goToIntro").attr("checked", true);
+$(".goDownBtn").scrollTop(sectionHeight);
+
+// $(document).on('scroll', function () {
+//     if ($(document).scrollTop() < sectionHeight) {
+//         $("#goToIntro").attr("checked", true)
+//     }
+//     else if ($(document).scrollTop() >= 1 * sectionHeight && $(document).scrollTop() < 2*sectionHeight) {
+//         $("#goToSolutions").attr("checked", true)
+//     }
+//     else if ($(document).scrollTop() >= 2 * sectionHeight && $(document).scrollTop() < 3*sectionHeight ) {
+//         $("#goToMembers").attr("checked", true)
+//     }
+//     else if ($(document).scrollTop() >= 3 * sectionHeight) {
+//         $("#goToAbout").attr("checked", true)
+//     }
+//     else {
+//         $("#goToIntro").attr("checked", true)
+//     }
+//     console.log($(document).scrollTop(), "?", sectionHeight);
+// });
+
+$(document).on('scroll', function () {
+    if ($(document).scrollTop() > 100) {
         $(".goTopBtn").fadeIn();
     }
-    else{
+    else {
         $(".goTopBtn").fadeOut();
     }
-})
+});
+
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+
+
