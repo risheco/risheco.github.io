7ab42d37d3bd3f07ecc0ab7a4df113bf0037b704
Amirmohammad Moradi <amirmohammad@irsapardaz.ir>
2016-08-13 13:45:36 +0430

    desktop done

--- js/index.js
+++ js/index.js
@@ -3,61 +3,30 @@ $(window).ready(setTimeout(function () {
     $(".content").css({"display": "block"});
 }, 6000));
 
+$('html, body').animate({
+    scrollTop: 0
+}, 0);
 function hoverLeft() {
-//     // alert('aaa');
-//     $("#web").css({'width': '55%'});
-//     $("#app").css({'width': '45%'});
-//     $(".slant-container").css({"margin-left": "5%"});
     $("#web").removeClass("decreaseWidth");
     $("#web").addClass("increaseWidth");
-    // $("#web.web-content").addClass("scaled");
     $("#app").removeClass("increaseWidth");
     $("#app").addClass("decreaseWidth");
-    // $("#app.app-content").removeClass("scaled");
 };
 function hoverRight() {
-//     // alert('aaa');
-//     $("#app").css({'width': '55%'});
-//     $("#web").css({'width': '45%'});
-//     $(".slant-container").css({"margin-right": "5%"});
     $("#app").removeClass("decreaseWidth");
     $("#app").addClass("increaseWidth");
-    // $("#app.app-content").addClass("scaled");
     $("#web").removeClass("increaseWidth");
     $("#web").addClass("decreaseWidth");
 };
-
-
 $("section#members ul.membersList li.member").on('mouseenter', function () {
     $(this).children(".memberInfo").stop().fadeIn();
 });
 $("section#members ul.membersList li.member").on('mouseleave', function () {
     $(this).children(".memberInfo").stop().hide();
 });
-
 var sectionHeight = $("section").height();
-$("#goToIntro").attr("checked", true);
+$("#goToIntro").prop("checked", true);
 $(".goDownBtn").scrollTop(sectionHeight);
-
-// $(document).on('scroll', function () {
-//     if ($(document).scrollTop() < sectionHeight) {
-//         $("#goToIntro").attr("checked", true)
-//     }
-//     else if ($(document).scrollTop() >= 1 * sectionHeight && $(document).scrollTop() < 2*sectionHeight) {
-//         $("#goToSolutions").attr("checked", true)
-//     }
-//     else if ($(document).scrollTop() >= 2 * sectionHeight && $(document).scrollTop() < 3*sectionHeight ) {
-//         $("#goToMembers").attr("checked", true)
-//     }
-//     else if ($(document).scrollTop() >= 3 * sectionHeight) {
-//         $("#goToAbout").attr("checked", true)
-//     }
-//     else {
-//         $("#goToIntro").attr("checked", true)
-//     }
-//     console.log($(document).scrollTop(), "?", sectionHeight);
-// });
-
 $(document).on('scroll', function () {
     if ($(document).scrollTop() > 100) {
         $(".goTopBtn").fadeIn();
@@ -66,12 +35,27 @@ $(document).on('scroll', function () {
         $(".goTopBtn").fadeOut();
     }
 });
-
+$('#goTopBtn').click(function () {
+    $("#goToIntro").prop("checked", true);
+    $('.sectionChanger').removeClass('blue');
+});
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target[0].id == "solutions") {
+                $("#goToSolutions").prop("checked", true);
+                $('.sectionChanger').removeClass('blue');
+            }
+            else if (target[0].id == "members") {
+                $("#goToMembers").prop("checked", true);
+                $('.sectionChanger').removeClass('blue');
+            }
+            else if (target[0].id == "about") {
+                $("#goToAbout").prop("checked", true);
+                $('.sectionChanger').addClass('blue');
+            }
             if (target.length) {
                 $('html, body').animate({
                     scrollTop: target.offset().top
@@ -81,31 +65,34 @@ $(function () {
         }
     });
 });
-
 $(function () {
     $('#goToIntro').click(function () {
         $('html, body').animate({
             scrollTop: $('#intro').offset().top
         }, 1000);
-        $("#goToIntro").attr("checked", true);
+        $("#goToIntro").prop("checked", true);
+        $('.sectionChanger').removeClass('blue');
     });
     $('#goToSolutions').click(function () {
         $('html, body').animate({
             scrollTop: $('#solutions').offset().top
         }, 1000);
-        $("#goToSolutions").attr("checked", true);
+        $("#goToSolutions").prop("checked", true);
+        $('.sectionChanger').removeClass('blue');
     });
     $('#goToMembers').click(function () {
         $('html, body').animate({
             scrollTop: $('#members').offset().top
         }, 1000);
-        $("#goToMembers").attr("checked", true);
+        $("#goToMembers").prop("checked", true);
+        $('.sectionChanger').removeClass('blue');
     });
     $('#goToAbout').click(function () {
         $('html, body').animate({
             scrollTop: $('#about').offset().top
         }, 1000);
-        $("#goToAbout").attr("checked", true);
+        $("#goToAbout").prop("checked", true);
+        $('.sectionChanger').addClass('blue');
     });
 });
 
